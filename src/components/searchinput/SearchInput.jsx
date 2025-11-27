4c049103138d0479c316f94cb9ef5433cbc92513
DecSeung <jungsi1217@gmail.com>
2025-11-27 12:18:45 +0900

    feat: 검색어 자동완성 키보드로 움직이는 기능 구현

--- src/components/searchinput/SearchInput.jsx
+++ src/components/searchinput/SearchInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Search } from "lucide-react";
 import SearchResult from "../searchresult/SearchResult";
 import "./SearchInput.css";
@@ -19,6 +19,8 @@ function SearchInput() {
   const debouncedFoodName = useDebounce(foodName, 300);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+
+  const inputRef = useRef();
 
   // 콘솔 로그는 상태가 실제로 업데이트 된 후 찍기
   useEffect(() => {
@@ -46,21 +48,33 @@ function SearchInput() {
       });
   }, [debouncedFoodName]);
 
+  const handleResultFocus = (e) => {
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      const firstCard = document.querySelector(
+        ".search-result-area .food-card"
+      );
+      if (firstCard) firstCard.focus();
+    }
+  };
+
   return (
     <div className="search-input-area">
       <div className="search-input-wrap">
         <span>식품 검색</span>
         <input
+          ref={inputRef}
           type="text"
           value={foodName}
           onChange={(e) => setFoodName(e.target.value)}
           placeholder="음식명을 입력하세요"
+          onKeyDown={handleResultFocus}
         />
         <div className="search-btn">
           <Search />
         </div>
       </div>
-      <SearchResult data={data} />
+      <SearchResult data={data} inputRef={inputRef} />
     </div>
   );
 }
--- src/components/searchresult/SearchResult.jsx
+++ src/components/searchresult/SearchResult.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./SearchResult.css";
 import { MoveRight } from "lucide-react";
+import * as RovingFocusGroup from "@radix-ui/react-roving-focus";
 
-function SearchResult({ data }) {
-  console.log(data);
+function SearchResult({ data, inputRef }) {
+  const rovingRef = useRef(null);
+
+  const handleInputFocus = (e) => {
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+
+      // 검색 결과가 없거나, 이 카드가 첫 번째 카드인 경우 input으로 포커스
+      const foodCards = document.querySelectorAll(
+        ".search-result-area .food-card"
+      );
+      if (foodCards.length === 1 || e.currentTarget === foodCards[0]) {
+        inputRef.current?.focus();
+      } else {
+        // 일반적으로 이전 카드로 이동
+        const prevCard = e.currentTarget.previousElementSibling;
+        prevCard?.focus();
+      }
+    }
+  };
+
   return (
-    <div className={`search-result-area ${data ? "open" : ""}`}>
-      {data?.response?.body?.items.map((item) => {
-        const filterFoodName = item.foodNm.replaceAll("_", " ");
-        return (
-          <div className="food-card" key={item.foodCd}>
-            <div className="food-name">{filterFoodName}</div>
-            <div className="kcal-area">
-              <span>1g / kcal</span>
-              <div className="kcal-info">{item.enerc / 100}</div>
-            </div>
-            <div className="enter-arrow">
-              <MoveRight size={18} />
-            </div>
+    <RovingFocusGroup.Root orientation="vertical" ref={rovingRef}>
+      <div className={`search-result-area ${data ? "open" : ""}`}>
+        {data?.response?.body?.items.map((item, index) => {
+          const filterFoodName = item.foodNm.replaceAll("_", " ");
+          return (
+            <RovingFocusGroup.Item key={item.foodCd} asChild>
+              <div
+                className="food-card"
+                tabIndex={0}
+                onKeyDown={handleInputFocus}
+              >
+                <div className="food-name">{filterFoodName}</div>
+                <div className="kcal-area">
+                  <span>1g / kcal</span>
+                  <div className="kcal-info">{item.enerc / 100}</div>
+                </div>
+                <div className="enter-arrow">
+                  <MoveRight size={18} />
+                </div>
+              </div>
+            </RovingFocusGroup.Item>
+          );
+        })}
+
+        <RovingFocusGroup.Item asChild key="direct">
+          <div
+            className="direct-insert food-card"
+            tabIndex={0}
+            onKeyDown={handleInputFocus}
+          >
+            직접 입력 하기
           </div>
-        );
-      })}
-      <div className="direct-insert food-card">직접 입력 하기</div>
-    </div>
+        </RovingFocusGroup.Item>
+      </div>
+    </RovingFocusGroup.Root>
   );
 }
 
